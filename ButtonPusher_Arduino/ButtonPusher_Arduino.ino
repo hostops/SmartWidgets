902172e695d41056093618ae320961f2c8a32b40
Jakob Hostnik <jakob.hostnik@gmail.com>
2017-05-12 15:58:59 +0200

    final update

---
+++ ButtonPusher_Arduino/ButtonPusher_Arduino.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+Servo pusher;
+void setup() {
+pinMode(13,OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  if (Serial.available() && Serial.read() == '\n') {
+    byte c = Serial.read();
+    if (c == 1) {
+      turnOn();
+    }else{
+    turnOff();
+    }
+    
+  }
+}
+
+void turnOn() {
+  pusher.attach(9);
+  pusher.write(180);
+  delay(1000);
+  pusher.write(0);
+  delay(1000);
+  pusher.detach();
+}
+void turnOff() {
+  pusher.attach(9);
+  pusher.write(180);
+  delay(1000);
+  pusher.write(0);
+  delay(1000);
+  pusher.detach();
+}
